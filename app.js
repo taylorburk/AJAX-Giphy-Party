e9bafee2c07840e519a4827ae5f72034c55efb7f
Taylor Burkhalter <96509561+taylorburk@users.noreply.github.com>
2023-04-13 22:08:12 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+function addGif(res) {
+  let numResults = res.data.length;
+  if (numResults) {
+    let randomIdx = Math.floor(Math.random() * numResults);
+    let $newCol = $("<div>", { class: "col-md-4 col-12 mb-4" });
+    let $newGif = $("<img>", {
+      src: res.data[randomIdx].images.original.url,
+      class: "w-100"
+    });
+    $newCol.append($newGif);
+    $("section").append($newCol);
+  }
+}
+
+$("form").on("submit", async function(evt) {
+  evt.preventDefault();
+  const $searchInput = $("#search");
+  let searchTerm = $searchInput.val();
+  $searchInput.val("");
+
+  const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: searchTerm,
+      api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+    }
+  });
+  addGif(response.data);
+});
+
+$("#remove").on("click", function() {
+  $("section").empty();
+});
